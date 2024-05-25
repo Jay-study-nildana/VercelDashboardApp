733a3689ebe7f24a19fd24269e3abc5cab57b7d0
Jay <vijayasimhabr@live.com>
2024-05-25 11:43:51 +0530

    step 7 completed
    display some data on the dashboard page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,6 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    //A common way to avoid waterfalls is to initiate all data requests at the same time - in parallel.
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
