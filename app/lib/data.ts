41d20ae321bd4265563a60840012431327ef29f0
Jay <vijayasimhabr@live.com>
2024-05-26 09:48:39 +0530

    step 9 completed
    step 10 is just theory
    
    so next will be step 11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -39,12 +39,20 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+
+    console.log('Fetching Latest Invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
+
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log('Data fetch Latest Invoices completed after 4 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -62,6 +70,8 @@ export async function fetchCardData() {
   noStore();
 
   try {
+
+    console.log('Fetching Card data...');
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
@@ -71,7 +81,6 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-
     //A common way to avoid waterfalls is to initiate all data requests at the same time - in parallel.
     const data = await Promise.all([
       invoiceCountPromise,
